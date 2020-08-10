c502dca96cb598d1052f2a5ff828321a36f37d10
Jessica <duellje@oregonstate.edu>
2020-08-10 14:10:01 -0400

    finished guided project

--- posts/post-router.js
+++ posts/post-router.js
@@ -21,10 +21,64 @@ router.get("/", (req, res) => {
 
 router.get("/:id", (req, res) => {});
 
-router.post("/", (req, res) => {});
+router.post("/", (req, res) => {
+  const post = req.body;
 
-router.put("/:id", (req, res) => {});
+  //shortcut instead of using .from()
+  db("posts")
+    .insert(post)
+    .returning("id") //if supporting PostgresSQL need this line
+    .then((ids) => {
+      // the warning: ".returning() is not supported by sqlite3 and will not have any effect."
+      // can safely be ignored when using SQLite
+      // it will go away when using PostgreSQL
+      res.status(201).json({ inserted: ids });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        errorMessage: err.message,
+      });
+    });
+});
 
-router.delete("/:id", (req, res) => {});
+router.put("/:id", (req, res) => {
+  const changes = req.body;
+  const postId = req.params.id;
+
+  db("posts")
+    .where({ id: postId })
+    // .where("id", "=", postId) - another way to write the where
+    .update(changes)
+    .then((count) => {
+      if (count) {
+        res.status(200).json({ message: "Updated Post successfully" });
+      } else {
+        res.status(404).json({ message: "Post with that ID not found" });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ errorMessage: err.message });
+    });
+});
+
+router.delete("/:id", (req, res) => {
+  const postId = req.params.id;
+  db("posts")
+    .where({ id: postId })
+    .delete()
+    .then((count) => {
+      if (count) {
+        res.status(200).json({ message: "Deleted Post Successfully" });
+      } else {
+        res.status(404).json({ message: "Post with that ID not found" });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ errorMessage: err.message });
+    });
+});
 
 module.exports = router;
